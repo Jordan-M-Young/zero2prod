195af6f90bd03172d1ccfd162f3dfa277e885db7
Jordan-M-Young <jordan.m.young0@gmail.com>
2022-12-06 20:39:41 -0600

    chapter 3 commits

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Helllllo {}!", &name)
+}
+ 
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    println!("starting server......");
+    let server = HttpServer::new( || {
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+
+
+}
