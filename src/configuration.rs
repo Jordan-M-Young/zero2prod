db44ddbd21311d5e6688e3566ac95d085cffc029
Jordan-M-Young <jordan.m.young0@gmail.com>
2022-12-13 20:37:37 -0600

    comment

--- src/configuration.rs
+++ src/configuration.rs
@@ -13,7 +13,7 @@ pub struct DatabaseSettings {
     pub database_name: String
 }
 
-
+//some comment
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
         .add_source(
